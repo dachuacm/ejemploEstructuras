17cc8ee562191814f675457c86e937e9a1382198
Aurora <diana.aurora.cruz@uacm.edu.mx>
2026-09-24 11:14:29 -0600

    actualizado

--- listaCanciones.c
+++ listaCanciones.c
@@ -4,14 +4,8 @@
 
 void main(){
 // lista de tus 5 canciones favoritas
-  int i;
-  Cancion favoritas[10];
+ 
 
-  for(i=0;i<TAM;i++){
-    agregarCancion(favoritas,i);
 
-  }
-
-  imprimir(favoritas);
 }
 
